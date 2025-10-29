a609b38946b8ffed28f9993d0e2189241ebe2dcd
Kaust22 <kaustubhkantrastogi2004@gmail.com>
2025-10-29 21:00:02 +0530

    configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Correct path to root HTML file
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
