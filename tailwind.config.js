c7bdf691e200f9ffd067c508dd67e0bc0e064653
Rahul Verma <167573663+RahulVerma2629@users.noreply.github.com>
2026-02-25 19:34:47 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Correct path to root HTML file
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'roboto-condensed': ['Roboto Condensed', 'sans-serif'],
+        'montserrat': ['Montserrat', 'sans-serif'],
+        'raleway': ['Raleway', 'sans-serif'],
+        'commissioner': ['Commissioner', 'sans-serif'],
+        'titillium': ['Titillium Web', 'sans-serif'],
+        'playfair': ['Playfair Display', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
